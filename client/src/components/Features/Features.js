c24069f51bdc5c7cebcb1f0148f5aa2b15d4f813
Taylor Martin <taylorrobertmartin@gmail.com>
2021-03-26 14:49:53 -0400

    header hooekd up

--- client/src/components/Features/Features.js
+++ client/src/components/Features/Features.js
@@ -1,33 +1,45 @@
-import './Features.scss';
-import FeaturesCard from '../FeaturesCard/FeaturesCard'; //8, 7, 5
-import featOne from '../../assets/images/feat-8.png';
-import featTwo from '../../assets/images/feat-7.png';
-import featThree from '../../assets/images/feat-5.png';
+import "./Features.scss";
+import FeaturesCard from "../FeaturesCard/FeaturesCard"; //8, 7, 5
+import featOne from "../../assets/images/feat-8.png";
+import featTwo from "../../assets/images/feat-7.png";
+import featThree from "../../assets/images/feat-5.png";
 
-const Features = ({heading, display}) => {
-    if(!(display)) {
+const Features = ({ heading, display }) => {
+  if (!display) {
     return (
-        <div className='features'>
-            <h2 className='features__heading'>{heading}</h2>
-            <div className='features__card-container'>
-                <FeaturesCard copyright='features-card__hidden' text='Get alerted when your appointment gets bumped up' img={featOne}/>
-                <FeaturesCard copyright='features-card__copyright' text='Confirm your appointment and get Fast Tracked' img={featTwo}/>
-                <FeaturesCard copyright='features-card__hidden' text='Use our maps to get to your local hospital on time' img={featThree}/>
-            </div>
+      <div className="features" id="features">
+        <h2 className="features__heading">{heading}</h2>
+        <div className="features__card-container">
+          <FeaturesCard
+            copyright="features-card__hidden"
+            text="Get alerted when your appointment gets bumped up"
+            img={featOne}
+          />
+          <FeaturesCard
+            copyright="features-card__copyright"
+            text="Confirm your appointment and get Fast Tracked"
+            img={featTwo}
+          />
+          <FeaturesCard
+            copyright="features-card__hidden"
+            text="Use our maps to get to your local hospital on time"
+            img={featThree}
+          />
         </div>
+      </div>
     );
-    } else {
-        return (
-        <div className='features'>
-        <h2 className='features__heading'>{heading}</h2>
-        <div className='features__card-container'>
-            <FeaturesCard copyright='features-card__hidden' text='' img=''/>
-            <FeaturesCard copyright='features-card__hidden' text='' img=''/>
-            <FeaturesCard copyright='features-card__hidden' text='' img=''/>
+  } else {
+    return (
+      <div className="features" id="resources">
+        <h2 className="features__heading">{heading}</h2>
+        <div className="features__card-container">
+          <FeaturesCard copyright="features-card__hidden" text="" img="" />
+          <FeaturesCard copyright="features-card__hidden" text="" img="" />
+          <FeaturesCard copyright="features-card__hidden" text="" img="" />
         </div>
-    </div>
-        );
-    }
+      </div>
+    );
+  }
 };
 
 export default Features;
